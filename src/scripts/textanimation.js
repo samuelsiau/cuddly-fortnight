0d1874491e14e85e1cdff61ef160bb577a6f1207
Samuel Siau <samuelsiau@gmail.com>
2023-11-01 14:36:59 +1300

    Updated

--- src/scripts/textanimation.js
+++ src/scripts/textanimation.js
@@ -6,11 +6,13 @@ gsap.registerPlugin(ScrollTrigger);
 
 const text = new SplitType("#textHero"), 
       tl = gsap.timeline(), 
-      tl2 = gsap.timeline(), 
+      tl2 = gsap.timeline(),
+      tl3 = gsap.timeline(),
       textLines = text.lines, 
       textHero2 = document.querySelector('#textHero2'),
-      text2 = new SplitType(textHero2),
-      textLines2 = text2.lines;
+      textHero3 = document.querySelector('#textHero3'),
+      text2 = new SplitType(textHero2).lines,
+      text3 = new SplitType(textHero3).words;
 
 
 function wrapElements() {
@@ -54,23 +56,39 @@ tl.fromTo(textLines, {
   delay: 1
 });
 
-gsap.set(textLines2, {
+gsap.set(text2, {
   y: 100,
   autoAlpha: 0,
 })
 
-tl2.fromTo(textLines2, {
+tl2.fromTo(text2, {
   autoAlpha: 0,
 }, {
   y: 0,
   autoAlpha: 1,
   ease: "back.out(1.4)",
   stagger: 0.17,
+  delay: 1,
+  duration: 0.7
+})
+
+tl3.fromTo(text3, {
+  y: 20,
+  autoAlpha: 0,
+}, {
+  y: 0,
+  autoAlpha: 1,
+  ease: "power1.out",
+  // duration: 0.3,
+  stagger: 0.13,
   delay: 1
 })
 
 document.getElementById("animateagain").onclick = function(){
   tl.restart();
   tl2.restart();
+  tl3.restart();
 };
+
+console.log(tl3)
 
